14db86cb83d930a7971bb2d9ab253962f5f9e734
dragon script <agomila96@gmail.com>
2020-10-07 22:28:28 +0200

    ➕ Replace Commander with CLI

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,12 +1,16 @@
-import {program} from 'commander'
+import cli from 'cli'
 import commands from './commands'
 
-export class cli {
+export class CLI {
     private args: string[]
-    public constructor() {
-        this.args = process.argv
+    public constructor(args: string[]) {
+        this.args = args
+        this.init()
     }
     public execute(command: string, args: string[]) {
+        
+    }
+    private init() {
         
     }
 }
--- src/cli/init.ts
+++ src/cli/init.ts
@@ -1,11 +1,3 @@
-import {program} from 'commander'
-import {cli} from './cli'
-import packageNPM from '../../package.json'
+import * as cli from './cli'
 
-export function init() {
-    program.parse(process.argv)
-    program.version(packageNPM.version)
-    program.option("--version, -v", "Prints the current CLI version.")
-    program.option("help", "")
-    program.option("publish", "Publishes the CWD to XStore if the package exists and it's yours.")
-}
+new cli.CLI(process.argv)
