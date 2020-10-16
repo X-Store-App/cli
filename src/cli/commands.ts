dbad4bb62efba3bb233f0ded710d12861b60436d
dragon script <agomila96@gmail.com>
2020-10-16 23:31:09 +0200

    🏷️ Fix types

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -1,10 +1,8 @@
 interface commands {
-    commands: [
-        {
-            title: string;
-            do: (args: string[]) => void;
-        }
-    ]
+    commands: {
+        title: string;
+        do: (args: string[]) => void;
+    }[]
 }
 
 const commands: commands = {
@@ -13,6 +11,18 @@ const commands: commands = {
             title: "version",
             do: (args: string[]) => {
                 console.log(process.version)
+            }
+        },
+        {
+            title: "create",
+            do: (args: string[]) => {
+                
+            }
+        },
+        {
+            title: "publish",
+            do: (args: string[]) => {
+                
             }
         }
     ]
