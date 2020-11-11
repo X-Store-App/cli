805d10117da23886c9633d330eb970ab8c5dfabe
dragonDScript <agomila96@gmail.com>
2020-11-11 19:15:50 +0100

    Add sample --version flag

--- source/main.py
+++ source/main.py
@@ -1,4 +1,18 @@
 import argparse
 
+# Program version and other data
+
+version = "0.1"
+
 parser = argparse.ArgumentParser()
-parser.parse_args()
+
+# Parse commands, arguments, etc
+
+parser.add_argument("-v", "--version", help="show the current XStore CLI version installed on your system", action="store_true")
+
+# Actions
+
+args = parser.parse_args()
+
+if args.version:
+  print(version)
