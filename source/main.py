a3984388946a2cb5db9f365c409c91dfe969d802
dragonDScript <agomila96@gmail.com>
2020-11-11 19:58:04 +0100

    Parse succesfully

--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
 import argparse
+import packageManager
 
 # Program version and other data
 
@@ -8,11 +9,18 @@ parser = argparse.ArgumentParser()
 
 # Parse commands, arguments, etc
 
+group = parser.add_argument_group()
+
+group.add_argument("install", help="install a xstore package")
+group.add_argument("remove", help="remove a xstore package")
+
 parser.add_argument("-v", "--version", help="show the current XStore CLI version installed on your system", action="store_true")
 
 # Actions
 
 args = parser.parse_args()
 
-if args.version:
-  print(version)
+if args.install:
+  pass
+elif args.remove:
+  pass
---
+++ source/packageManager.py
@@ -0,0 +1,5 @@
+def install():
+  pass
+
+def remove():
+  pass
